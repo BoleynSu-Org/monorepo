d7c2b4318c468fdcae9131da4bba84940eced3ee
Git Repo Syncer <bot.boleyn.su+git-repo-syncer@gmail.com>
2024-03-12 23:53:14 +0800

    [Internal] boleynsu_org: Use bzlmod by default
    Internal-Path: third_party/boleynsu_org
    Internal-Commit-Id: 9704c608a0521981deff40491f64cc6279a5a153

--- cmd/infra/deps/update.sh
+++ cmd/infra/deps/update.sh
@@ -8,13 +8,13 @@ if [[ -v BUILD_WORKSPACE_DIRECTORY ]]; then
   unset BUILD_WORKING_DIRECTORY
 fi
 
-REPIN=1 bazel run --lockfile_mode=update //configs/bazel:deps_bzl.genfile
-REPIN=1 bazel run --lockfile_mode=update //configs/deps:deps_yaml.genfile
-REPIN=1 bazel run --lockfile_mode=update //configs/bazel:deps_bzl.genfile
-REPIN=1 bazel run --lockfile_mode=update @unpinned_maven//:pin
-REPIN=1 bazel run --lockfile_mode=update @unpinned_pip//:pin
-REPIN=1 bazel run --lockfile_mode=update @unpinned_gazelle_go_deps//:pin
-REPIN=1 bazel run --lockfile_mode=update //third_party/io_grpc_grpc_java:module_bazel.genfile
-REPIN=1 bazel run --lockfile_mode=update //.prow:presubmit.genfile
-REPIN=1 bazel run --lockfile_mode=update //.prow:postsubmit.genfile
-REPIN=1 bazel run --lockfile_mode=update //oj/oj-server:versions.genfile
+REPIN=1 bazel run --config=disable_bzlmod //configs/bazel:deps_bzl.genfile
+REPIN=1 bazel run --config=disable_bzlmod //configs/deps:deps_yaml.genfile
+REPIN=1 bazel run --config=disable_bzlmod //configs/bazel:deps_bzl.genfile
+REPIN=1 bazel run --config=disable_bzlmod @unpinned_maven//:pin
+REPIN=1 bazel run --config=disable_bzlmod @unpinned_pip//:pin
+REPIN=1 bazel run --config=disable_bzlmod @unpinned_gazelle_go_deps//:pin
+REPIN=1 bazel run --config=disable_bzlmod //third_party/io_grpc_grpc_java:module_bazel.genfile
+REPIN=1 bazel run --config=disable_bzlmod //.prow:presubmit.genfile
+REPIN=1 bazel run --config=disable_bzlmod //.prow:postsubmit.genfile
+REPIN=1 bazel run --config=disable_bzlmod //oj/oj-server:versions.genfile
