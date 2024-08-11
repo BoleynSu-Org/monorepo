b47e26b3f9e44a754acce89b29e864853cb9d894
Git Repo Syncer <bot.boleyn.su+git-repo-syncer@gmail.com>
2024-08-11 11:23:50 +0800

    [Internal] update deps
    Internal-Path: third_party/boleynsu_org
    Internal-Commit-Id: 8f6e38dea21f77d54cd8e78b985209e6740f4bf0

--- cmd/infra/deps/update.sh
+++ cmd/infra/deps/update.sh
@@ -10,12 +10,13 @@ fi
 
 export REPIN=1
 
-bazel run --lockfile_mode=update //configs/deps:deps_yaml.genfile
-bazel run --lockfile_mode=update //:bazelversion.genfile
-bazel run --lockfile_mode=update @unpinned_maven//:pin
-bazel run --lockfile_mode=update @unpinned_pip//:pin
-bazel run --lockfile_mode=update //third_party/io_grpc_grpc_java:module_bazel.genfile
-bazel run --lockfile_mode=update //.prow:presubmit.genfile
-bazel run --lockfile_mode=update //.prow:postsubmit.genfile
-bazel run --lockfile_mode=update //oj/oj-server:versions.genfile
+bazel run --lockfile_mode=off //configs/deps:deps_yaml.genfile
+bazel run --lockfile_mode=off //:bazelversion.genfile
+bazel run --lockfile_mode=off @unpinned_maven//:pin
+bazel run --lockfile_mode=off @unpinned_pip//:pin
+bazel run --lockfile_mode=off //third_party/io_grpc_grpc_java:module_bazel.genfile
+bazel run --lockfile_mode=off //.prow:presubmit.genfile
+bazel run --lockfile_mode=off //.prow:postsubmit.genfile
+bazel run --lockfile_mode=off //oj/oj-server:versions.genfile
+
 bazel mod graph --lockfile_mode=update
