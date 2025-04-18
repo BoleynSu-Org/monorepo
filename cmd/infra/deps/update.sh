16d2042b0d4e93243041ce69a5014a77053eb141
Git Repo Syncer <bot.boleyn.su+git-repo-syncer@gmail.com>
2025-04-18 21:51:57 +0800

    [Internal] io_grpc_grpc_java: Use upstream MODULE.bazel
    Internal-Path: third_party/boleynsu_org
    Internal-Commit-Id: face6cabdcb96c341409da0f38f042448f9fc75b

--- cmd/infra/deps/update.sh
+++ cmd/infra/deps/update.sh
@@ -10,11 +10,9 @@ fi
 
 export REPIN=1
 
-bazel run --lockfile_mode=off //configs/deps:deps_yaml.genfile
 bazel run --lockfile_mode=off //:bazelversion.genfile
 bazel run --lockfile_mode=off @unpinned_maven//:pin
 bazel run --lockfile_mode=off @unpinned_pip//:pin
-bazel run --lockfile_mode=off //third_party/io_grpc_grpc_java:module_bazel.genfile
 bazel run --lockfile_mode=off //.prow:presubmit.genfile
 bazel run --lockfile_mode=off //.prow:postsubmit.genfile
 bazel run --lockfile_mode=off //oj/oj-server:versions.genfile
